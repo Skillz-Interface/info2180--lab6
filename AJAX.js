3565323df2acc06f1b10a4b2a9895824ba35821d
Shanell Brown <shanellbrown332@gmail.com>
2017-11-01 13:52:07 -0700

    Exercise1

---
+++ AJAX.js
@@ -0,0 +1,32 @@
+"use strict";
+function showDef(){
+				var query = document.getElementById("queryTxtBox").value.toLowerCase();
+				var xhttp = new XMLHttpRequest();
+
+
+				else{
+					var xmlhttp= new XMLHttpRequest();
+					xmlhttp.onreadystatechange = function(){
+						if (this.readyState==4 && this.status == 200){
+							Search(this,Search,query)
+							
+						}
+					}
+				};
+			
+			xmlhttp.open("GET", "request.php",true);
+			xmlhttp.send();
+
+		}
+		function Search(xml,Search,query){
+			var xmlDoc = xml.resonseXML;
+			var x = xmlDoc.getElementsByTagName("definition")
+			var def;
+
+			if(Search){
+
+				def = x[1].chiildNodes[0].nodeValue
+
+				alert(def)
+			}
+		},;
